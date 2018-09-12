f1cec2a6cb05f483a65d832dd48fd97c3875224a
Shrey Pandya <shrey150@yahoo.com>
2018-09-12 14:06:54 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Shrey Pandya p7
+ * Smiley Face
+ *
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| * * |");
+		System.out.println("|  *  |");
+		System.out.println("| \\_/ |");
+		System.out.println("\\_____/");
+	}
+
+}
